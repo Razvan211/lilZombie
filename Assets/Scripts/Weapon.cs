04ba562615fd5145a24e6ba4e4ff30f57f52d45d
Razvan211 <72150650+Razvan211@users.noreply.github.com>
2022-05-23 04:20:28 +0300

    Muzzle added

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,6 +7,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30;
+    [SerializeField] ParticleSystem MuzzleFlash;
     public PlayerInput player;
    
     
@@ -19,20 +20,31 @@ public class Weapon : MonoBehaviour
     }
      private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
 
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        MuzzleFlash.Play();
+    }
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             Debug.Log("I hit " + hit.transform.name);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();  //call a method on EnemyHealth that decreases enemy's health
+            if (target == null) return;
             target.Damage(damage);
         }
         else
         {
             return;
         }
-
     }
+
     private void OnEnable()
     {
 
